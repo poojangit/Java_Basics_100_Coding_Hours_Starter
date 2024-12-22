a36df491c8ca9d6c08ace744a683252d8a833c02
poojangit <poojang147@gmail.com>
2024-12-22 20:22:31 +0530

    [POOJA] Added : program that determines if a number is odd or even using the biwise AND operator

---
+++ Operators_Programs/Biwise_Operators/OddOrEven.java
@@ -0,0 +1,20 @@
+//todo : program that determines if a number is odd or even using the bitwise AND operator
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void checkOddEven(int n){
+        if((n&1)==0){
+            System.out.println(n + " is a even number");
+        }
+        else {
+            System.out.println(n +  " is a odd number");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        checkOddEven(n);
+    }
+}
