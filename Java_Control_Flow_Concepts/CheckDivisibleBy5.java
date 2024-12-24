1106ea9f49ab93f494f2371a259921b6937b9740
poojangit <poojang147@gmail.com>
2024-12-24 15:13:43 +0530

    [POOJA] Added : solved 6 problems on control flow

---
+++ Java_Control_Flow_Concepts/CheckDivisibleBy5.java
@@ -0,0 +1,20 @@
+//todo : Write a program to check if a number is divisible by 5
+//! I/P => number
+//! O/P => Is the number ___ divisible by 5? _
+
+import java.util.Scanner;
+
+public class CheckDivisibleBy5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        //checking true or false using if loop.
+        if(num%5 == 0){
+            System.out.println("Number is divisible by 5");
+        }
+        else
+        System.out.println("Number is not divisible");
+    }
+}
