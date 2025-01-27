f949276508b9b5d4dfb39451ec2e20d38ddd562e
poojangit <poojang147@gmail.com>
2025-01-27 12:04:32 +0530

    [POOJA] added : solved three basic level of programs to start with java

---
+++ Programs_To_Startwith/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+//2. Add Two Numbers
+//Write a program that takes two numbers as input from the user and prints
+//their sum.
+import java.util.Scanner;
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any two numbers: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum of two numbers : " + sum);
+    }
+}
---
+++ Programs_To_Startwith/Welcome.java
@@ -0,0 +1,8 @@
+//1. Welcome to Bridgelabz!
+//Write a program that prints "Welcome to Bridgelabz!" to the screen.
+
+class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BridgeLabz!");
+    }
+}
